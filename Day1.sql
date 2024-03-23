a31bd2301916c192ab6979f6c156ac0c319bc1a8
Rachanavk <65361077+Rachanavk@users.noreply.github.com>
2024-03-23 15:40:11 -0400

    Create Day1.sql

---
+++ Day1.sql
@@ -0,0 +1,6 @@
+SELECT * from CUSTOMERS;
+Practice Questions:
+1. INSERT INTO Customers(first_name,last_name,age,country) Values('Mat','Sip','32','India');
+Select * from Customers;
+2. Update Customers SET country='USA' where customer_id=6;
+3. Delete from Orders Where amount=400
